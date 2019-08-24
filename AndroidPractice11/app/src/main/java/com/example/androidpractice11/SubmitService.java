47abe73350fe4f3d77bb5f70aaa4b4378d52d43b
Park-SM <park97.sm@gmail.com>
2019-08-24 23:59:28 +0900

    Debugging for file uploading

--- AndroidPractice11/app/src/main/java/com/example/androidpractice11/SubmitService.java
+++ AndroidPractice11/app/src/main/java/com/example/androidpractice11/SubmitService.java
@@ -4,7 +4,16 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
-public class SubmitService extends AsyncTask<String, Void, Void> {
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.annotation.Target;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class SubmitService extends AsyncTask<String, Void, String> {
 
     private Context context;
 
@@ -13,16 +22,58 @@ public class SubmitService extends AsyncTask<String, Void, Void> {
     }
 
     @Override
-    protected Void doInBackground(String... args) {
+    protected String doInBackground(String... args) {
+        try {
+            String targetPath = (String) args[0];
+            String link = "http://smparkworld.com/AndroidViewerTest_insertIMG.php";
+            String lineEnd = "\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+
+            File targetImage = new File(targetPath);
+            FileInputStream fis = new FileInputStream(targetImage);
+
+            URL url = new URL(link);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data");
+            conn.setRequestProperty("imageName", targetPath);
+
+            DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"imageName\";filename=\"" + targetPath + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
 
 
+            final int MAXBUF = 1 * 1024 * 1024;
+            byte[] buffer = new byte[MAXBUF];
+            int ReadSize, BufferSize, TargetSize;
 
-        return null;
+            do {
+                TargetSize = fis.available();
+                BufferSize = Math.min(TargetSize, MAXBUF);
+                ReadSize = fis.read(buffer, 0, BufferSize);
+                dos.write(buffer, 0, BufferSize);
+            } while(ReadSize > 0);
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            Scanner sc = new Scanner(conn.getInputStream());
+            String resString = sc.nextLine();
+
+            return new String("Success: " + resString);
+        } catch (Exception e) {
+            return new String("Error: " + e.getMessage());
+        }
     }
 
     @Override
-    protected void onPostExecute(Void values) {
-        Toast.makeText(context,"Test toast", Toast.LENGTH_SHORT).show();
+    protected void onPostExecute(String result) {
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
     }
 
 }
